516d3034743ee820a87b6036bbb4f97684ff99df
Giancarlo Ventura <khan.maytok@gmail.com>
2021-05-19 21:01:53 -0500

    art

--- index.js
+++ index.js
@@ -201,8 +201,12 @@ app.post('/orthoray/', (req, res) => {
             printer.table([el.quantity, el.product_name, el.total])
         })
         printer.println(printLines()); //----------------------------------
-        printer.println(`IGV      : S/ ${body.igv}`);
-        printer.println(`SUBTOTAL : S/ ${body.igv}`);
+
+        if(body.serie.startsWith('F')){
+            printer.println(`IGV      : S/ ${body.igv}`);
+            printer.println(`SUBTOTAL : S/ ${body.subtotal}`);
+        }
+
         printer.println(`TOTAL    : S/ ${body.total}`);
         printer.println(`SON: ${numeroALetras(body.total)}`);
         printer.alignLeft();
