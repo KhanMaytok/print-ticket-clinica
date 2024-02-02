804f78785d02cba39807e4329bb9a2df81fd7e8d
Giancarlo Ventura <khan.maytok@gmail.com>
2024-02-02 10:51:59 -0500

    kogo

--- install.bat
+++ install.bat
@@ -1,16 +1,23 @@
 @echo off
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
+echo "Removiendo carpeta de módulos antigua..."
 rmdir /Q/S node_modules
+echo "Reiniciando repo..."
 git init
 git remote add origin https://github.com/KhanMaytok/print-ticket-clinica.git
 git add --all
 git commit -m "juajua"
 git fetch --all
+echo "Reseteando a master..."
 git reset --hard origin/master
+echo "Trayendo cambios..."
 git pull origin master
+echo "Instalando printer..."
 npm install printer --build-from-source --legacy-peer-deps
+echo "Instalando nodemon..."
 npm install -g nodemon
+echo "Instalando dependencias..."
 npm install
 
 echo " _______        _       _           _        _           _       "
