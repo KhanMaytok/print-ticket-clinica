d7b19748f8d0cf5f349e40ef91048f93f1629e7a
Giancarlo Ventura <khan.maytok@gmail.com>
2024-02-02 10:46:25 -0500

    cogoyarou

---
+++ iniciar.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo "CERRANDO OTROS PROCESOS DE IMPRESIÓN"
+taskkill /f /im node.exe
+
+echo "COMPROBANDO ACTUALIZACIONES"
+call git pull origin master
+call nodemon index.js
+
+echo " _____       _      _           _       "
+echo "|_   _|     (_)    (_)         | |      "
+echo "  | |  _ __  _  ___ _  __ _  __| | ___  "
+echo "  | | | '_ \| |/ __| |/ _` |/ _` |/ _ \ "
+echo " _| |_| | | | | (__| | (_| | (_| | (_) |"
+echo "|_____|_| |_|_|\___|_|\__,_|\__,_|\___/ "
+
+pause
